fea60509a751309271cc53ba96b881a8f4d6cba8
Cuffcas <41876119+Cuffcas@users.noreply.github.com>
2018-07-29 21:40:38 +0200

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,5 +1,19 @@
 Config                          = {}
-Config.Locale = 'en'
+Config.Locale = 'fr'
+
+Config.chien = 50000
+Config.chat = 15000
+Config.singe = 8000
+Config.loup = 30000
+Config.lapin = 25000
+Config.husky = 35000
+Config.cochon = 10000
+Config.caniche = 50000
+Config.carlin = 5000
+Config.retriever = 10000
+Config.berger = 55000
+Config.westie = 50000
+
 Config.Zones = {
   PetShop = {
     Pos   = {x = 562.19805908203,y = 2741.3090820313,z = 41.868915557861 },
